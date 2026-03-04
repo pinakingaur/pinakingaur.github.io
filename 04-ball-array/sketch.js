2d56c4dd1a8a1db09a22583fa61a48bcc3e55b58
pinakingaur <pinakingaur11@gmail.com>
2026-03-04 15:20:21 -0600

    Update sketch.js

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,35 +4,51 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
   background(220);
-<<<<<<< HEAD
-=======
 
   for (let ball of ballArray) {
     //move
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    // teleport if needed
+    if (ball.x - ball.radius > width) {
+      ball.x = -ball.radius; 
+    }
+    if (ball.x + ball.radius < 0) {
+      ball.x = width + ball.radius;
+    }
+    if (ball.y - ball.radius > height) {
+      ball.y = -ball.radius;
+    }
+    if (ball.y + ball.radius < 0) {
+      ball.y = height + ball.radius;
+    }
+
     //display
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius * 2);
   }
->>>>>>> 72a31635faeae520541cd287039deabcc77b9c81
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let theBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 40),
+    r: random(255),
+    g: random(255),
+    b: random(255),
   };
   ballArray.push(theBall);
 }
