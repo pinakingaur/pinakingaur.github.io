c1ae8c49bf2b19dfda292b2bd0b05fe81ebf24b8
pinakingaur <pinakingaur11@gmail.com>
2026-03-12 14:07:21 -0600

    added ball notation

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,7 +5,6 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
 let poolImg;
 
 // Preloading all the images used in the coding
@@ -20,8 +19,20 @@ function setup() {
 
 function draw() {
   background(0, 0, 128);
-  image(poolImg, width/2, height/2, poolImg.width*4, poolImg.height*4);
-  
+  image(poolImg, width/2, height/2, poolImg.width*4, poolImg.height*4);  
+}
+
+function spawnBall(_x, _y) {
+  let theBall = {
+    x: _x,
+    y: _y,
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    radius: random(10, 40),
+    r: random(255),
+    g: random(255),
+    b: random(255),
+  };
 }
 
 
