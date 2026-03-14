fb734a85520df47588773858359a44635e02e851
pinakingaur <pinakingaur11@gmail.com>
2026-03-14 10:24:47 -0600

    Made pool table boundaries and pockets

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,9 +5,36 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let mouseprevX = 0;
-let mouseprevY = 0;
 let poolImg;
+let matter;  
+let balls = [];
+let cueBall; 
+let aimLine; 
+const ballRadius = 10;
+const cueBallOrigin = 500;
+
+// hitbox
+const pocket = 4;
+const rim = 40; 
+
+// NEED help commenting
+const table = {
+  left: 160 - rim,
+  top: 395 - rim,
+  right: 1116,
+  bottom: 908,
+  boundaries: [],
+  pockets: [],
+  tableWidth: function () {
+    return this.right - this.left;
+  },
+  tableHeight: function () {
+    return this.bottom - this.top;
+  },
+  centerY: function () {
+    return this.top + this.tableHeight() / 2;
+  },   
+};
 
 // Preloading all the images used in the coding
 function preload() {
@@ -15,45 +42,25 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+   matter = Matter.Engine.create();
+  //  matter.world.gravity.y = 0;
   imageMode(CENTER);
+  Matter.Runner.run(matter); 
 }
 
 function draw() {
-  // background(0, 0, 40);
-  // image(poolImg, width/2, height/2, poolImg.width*4, poolImg.height*4);  
   background(220);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, mouseDragged, mouseDragged);
+  
+  
+  // translate the origin back to 0,0
+  translate(-width / 2,-height / 2);
+
+  // loads pool table
+  image(poolImg, width/2, height/2, poolImg.width*4, poolImg.height*4);  
+  
   
   }
-}
-
-function mouseClicked() {
-  // line(mouseprevX, mouseprevY, mouseX, mouseY);
-  
-  // Update previous positions for next click
-  mouseprevX = mouseX;
-  mouseprevY = mouseY;
-}
-
-
-
-
-
-// function spawnBall(_x, _y) {
-//   let theBall = {
-//     x: _x,
-//     y: _y,
-//     // dx: random(-5, 5),
-//     // dy: random(-5, 5),
-//     radius: 20,
-//   };
-// }
-
-// function mouseDragged() {
-// }
-
 
 
 
