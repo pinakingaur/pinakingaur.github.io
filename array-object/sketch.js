cb6c1be71a3f30b8c27d8aa871317aca1592e111
pinakingaur <pinakingaur11@gmail.com>
2026-03-15 12:08:19 -0600

    Boundary lines completed

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -10,11 +10,11 @@ let matter;
 let balls = [];
 let cueBall; 
 let aimLine; 
-const ballRadius = 10;
+const ballRadius = 20;
 const cueBallOrigin = 500;
 
 // hitbox
-const pocket = 4;
+const pocketToBallRatio = 4;
 const rim = 40; 
 
 let debugMode = false;
@@ -27,7 +27,7 @@ class hitBox {
    this.h = h; 
    let cx = x + w / 2;
    let cy = y + h / 2;
-   this.body = Matter.bodies.rectangles(cx, cy, w, h, {
+   this.body = Matter.Bodies.rectangle(cx, cy, w, h, {
     isStatic: true,
    });
    Matter.World.add(matter.world, this.body);
@@ -36,12 +36,13 @@ class hitBox {
 
 // NEED help commenting
 const table = {
-  left: 160 - rim,
-  top: 395 - rim,
-  right: 1116,
-  bottom: 908,
+  left: 35,
+  top: 300,
+  right: 1121,
+  bottom: 861,
   boundaries: [],
   pockets: [],
+
   tableWidth: function () {
     return this.right - this.left;
   },
@@ -59,7 +60,18 @@ const table = {
       new hitBox(this.right, this.top, rim, this.tableHeight() + rim),
    ];
   },
+  pocketHoles: function() {
+    this.pockets = [
+      createVector(59, 322),
+      createVector(591, 322),
+      createVector(1133, 322),
+      createVector(1133, 877),
+      createVector(591, 877),
+      createVector(59, 877),
+    ];
+  },
 };
+
 
 // Preloading all the images used in the coding
 function preload() {
@@ -83,14 +95,26 @@ function drawDebug() {
     rect(b.x, b.y, b.w, b.h);
   }
   pop();
+
+  // pockets
+  push();
+  fill("yellow");
+  noStroke();
+  table.pockets.forEach((pocket) => {
+    let r = ballRadius * pocketToBallRatio;
+    ellipse(pocket.x, pocket.y, r, r);
+  });
+  pop();
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(1200, 1200, WEBGL);
    matter = Matter.Engine.create();
   //  matter.world.gravity.y = 0;
 
+  // table.updateBounds();
   table.boundariesLine();
+  table.pocketHoles();
   imageMode(CENTER);
   Matter.Runner.run(matter); 
 }
@@ -106,9 +130,7 @@ function draw() {
   image(poolImg, width/2, height/2, poolImg.width*4, poolImg.height*4);  
 
   drawDebug();
-  
-  
-  }
+}
 
 
 
