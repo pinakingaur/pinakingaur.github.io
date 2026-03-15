a5171bcf2b65e687ca63bb59488d1363b64e4146
pinakingaur <pinakingaur11@gmail.com>
2026-03-15 11:15:27 -0600

    adding boundaries line

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -17,6 +17,23 @@ const cueBallOrigin = 500;
 const pocket = 4;
 const rim = 40; 
 
+let debugMode = false;
+
+class hitBox {
+  constructor(x, y, w, h) {
+   this.x = x;
+   this.y = y;
+   this.w = w;
+   this.h = h; 
+   let cx = x + w / 2;
+   let cy = y + h / 2;
+   this.body = Matter.bodies.rectangles(cx, cy, w, h, {
+    isStatic: true,
+   });
+   Matter.World.add(matter.world, this.body);
+   }
+  }
+
 // NEED help commenting
 const table = {
   left: 160 - rim,
@@ -33,7 +50,15 @@ const table = {
   },
   centerY: function () {
     return this.top + this.tableHeight() / 2;
-  },   
+  },  
+   boundariesLine: function () {
+    this.boundaries = [
+      new hitBox(this.left, this.top, this.tableWidth(), rim),
+      new hitBox(this.left, this.bottom, this.tableWidth(), rim),
+      new hitBox(this.left, this.top, rim, this.tableHeight()),
+      new hitBox(this.right, this.top, rim, this.tableHeight() + rim),
+   ];
+  },
 };
 
 // Preloading all the images used in the coding
@@ -41,10 +66,31 @@ function preload() {
   poolImg = loadImage("table.png");
 }
 
+function keyPressed() {
+  if (key === "d") debugMode = !debugMode;
+}
+
+function drawDebug() {
+  if (!debugMode) return;
+
+  push();
+  stroke("blue");
+  strokeWeight(3);
+  let c = color("pink");
+  c.setAlpha(100),
+  fill(c);
+  for (let b of table.boundaries) {
+    rect(b.x, b.y, b.w, b.h);
+  }
+  pop();
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
    matter = Matter.Engine.create();
   //  matter.world.gravity.y = 0;
+
+  table.boundariesLine();
   imageMode(CENTER);
   Matter.Runner.run(matter); 
 }
@@ -58,6 +104,8 @@ function draw() {
 
   // loads pool table
   image(poolImg, width/2, height/2, poolImg.width*4, poolImg.height*4);  
+
+  drawDebug();
   
   
   }
