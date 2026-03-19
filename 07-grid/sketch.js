d0555e18f488aa2295000a2e3fb6bba6aaada700
pinakingaur <pinakingaur11@gmail.com>
2026-03-19 13:04:30 -0600

    added toggle switch/ random grid

--- 07-grid/sketch.js
+++ 07-grid/sketch.js
@@ -1,12 +1,16 @@
 // 2D Grid Demo
 // Learning 2D arrays
 
-let theGrid = [[1, 0, 0], 
-              [1, 0, 1, 0],
-              [0, 1, 0, 0],
-              [0, 0, 1, 1]];
-const SQUARE_DIMENSION = theGrid.length;
+// use this if hard coding the grid
+// let theGrid = [[0, 0, 1, 0], 
+//               [1, 0, 1, 0],
+//               [0, 1, 1, 0],
+//               [0, 1, 0, 1]];
+// const SQUARE_DIMENSION = theGrid.length;
 
+// use this if randomizing the grid
+const SQUARE_DIMENSION = 10;
+let theGrid;
 let cellSize;
 
 function setup() {
@@ -17,11 +21,29 @@ function setup() {
   else {
     cellSize = width/SQUARE_DIMENSION;
   }
+  theGrid = randomizeGrid(SQUARE_DIMENSION, SQUARE_DIMENSION);
 }
 
 function draw() {
   background(220);
   showGrid();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  // console.log(x, y);
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  if (theGrid[y][x] === 1) {
+    theGrid[y][x] = 0;
+  }
+  else if (theGrid[y][x] === 0) {
+    theGrid[y][x] = 1;
+  }
 }
 
 function showGrid() {
@@ -38,9 +60,18 @@ function showGrid() {
   }
 }
 
-function changeColor() {
-  if (mouseIsPressed === true && theGrid[y][x] === 0) {
-    theGrid[y][x] === 1
+function randomizeGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        newGrid[y].push(0);
+      }
+      else {
+        newGrid[y].push(1);
+      }
+    }
   }
-  
+  return newGrid;
 }
