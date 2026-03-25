0c3114c954b4fbfe2561125c83ac43749f14bef3
pinakingaur <pinakingaur11@gmail.com>
2026-03-25 15:24:39 -0600

    hard coding grids

--- twoD-array/sketch.js
+++ twoD-array/sketch.js
@@ -6,32 +6,67 @@
 // - describe what you did to take this project "above and beyond"
 
 // create othello
-const CELL_SIZE = 20;
-let grid;
+// const CELL_SIZE = 150;
 let rows;
 let cols;
+let grid = [[0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0]];
+const CELL_SIZE = grid.length;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rows = Math.floor(height/CELL_SIZE);
-  cols = Math.floor(width/CELL_SIZE);
+  rows = Math.floor(CELL_SIZE);
+  cols = Math.floor(CELL_SIZE);
   grid = generateEmptyGrid(cols, rows);
 }
 
 function draw() {
   background(220);
+  displayGrid();
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+
+  // self
+  toggleCell(x, y);
 }
 
 function keyPressed() {
-  if (keyPressed === "e") {
+  if (key === "e") {
     grid = generateEmptyGrid(cols, rows);
   }
+}
+
+function toggleCell(x, y) {
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+    else if (grid[y][x] ===1) {
+      grid[y][x] = 0;
+    }
+  }
+  
 }
 
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      grid[y][x];
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
   }
 }
