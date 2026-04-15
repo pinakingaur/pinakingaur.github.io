5df17ff9b4b79d89f07d30991d6b3d010c4079e3
pinakingaur <pinakingaur11@gmail.com>
2026-04-15 14:49:34 -0600

    Added Fireworks

---
+++ 13-fireworks/sketch.js
@@ -0,0 +1,52 @@
+// Firework OOP Demo
+
+class Particle { 
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.dx = random(-5, 5);
+    this.dy = random(-5, 5);
+    this.radius = 3;
+    this.r = 255;
+    this.g = 0;
+    this.b = 0;
+    this.opacity = 255;
+  }
+
+  display() {
+    noStroke();
+    fill(this.r, this.g, this.b, this.opacity);
+    circle(this.x, this.y, this.radius * 2);
+  }
+
+  update() {
+    // MOVE
+    this.x += this.dx;
+    this.y += this.dy;
+    
+    // FADE AWAY OVER TIME
+    this.opacity--;
+  }
+}
+
+let theFireworks = [];
+const NUMBER_OF_PARTICLES_PER_CLICK = 100;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  for (let aFirework of theFireworks) {
+    aFirework.update();
+    aFirework.display();
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < NUMBER_OF_PARTICLES_PER_CLICK; i++) {
+    let someFirework = new Particle(mouseX, mouseY);
+    theFireworks.push(someFirework);
+  }
+}
